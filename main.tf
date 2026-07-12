c0247f108d98afa36ca488f5888280859d2c123c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:33 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+data "azurerm_key_vault_secret" "api_key" {
+  for_each     = { for k, v in var.stream_analytics_output_functions : k => v if v.api_key_key_vault_id != null && v.api_key_key_vault_secret_name != null }
+  name         = each.value.api_key_key_vault_secret_name
+  key_vault_id = each.value.api_key_key_vault_id
+}
 resource "azurerm_stream_analytics_output_function" "stream_analytics_output_functions" {
   for_each = var.stream_analytics_output_functions
 
-  api_key                   = each.value.api_key
+  api_key                   = each.value.api_key != null ? each.value.api_key : try(data.azurerm_key_vault_secret.api_key[each.key].value, null)
   function_app              = each.value.function_app
   function_name             = each.value.function_name
   name                      = each.value.name
--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ variable "stream_analytics_output_functions" {
 Map of stream_analytics_output_functions, attributes below
 Required:
     - api_key
+    - api_key_key_vault_id (alternative to api_key - read from Key Vault instead)
+    - api_key_key_vault_secret_name (alternative to api_key - read from Key Vault instead)
     - function_app
     - function_name
     - name
@@ -14,14 +16,16 @@ Optional:
 EOT
 
   type = map(object({
-    api_key                   = string
-    function_app              = string
-    function_name             = string
-    name                      = string
-    resource_group_name       = string
-    stream_analytics_job_name = string
-    batch_max_count           = optional(number) # Default: 100
-    batch_max_in_bytes        = optional(number) # Default: 262144
+    api_key                       = string
+    api_key_key_vault_id          = optional(string)
+    api_key_key_vault_secret_name = optional(string)
+    function_app                  = string
+    function_name                 = string
+    name                          = string
+    resource_group_name           = string
+    stream_analytics_job_name     = string
+    batch_max_count               = optional(number) # Default: 100
+    batch_max_in_bytes            = optional(number) # Default: 262144
   }))
 }
 
